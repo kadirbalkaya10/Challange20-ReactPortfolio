b4eb5bae00930f874b28c32eccfa9c6ea61d8c68
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2024-05-13 22:52:18 -0400

    Rendered Pages through App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import { Outlet } from "react-router-dom";
+
 function App() {
-  return <></>;
+  return (
+    <div className='container'>
+      <Outlet />
+    </div>
+  );
 }
 
 export default App;
